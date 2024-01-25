4b961120b6caa3d1d4adc9dfc0dbabc482e8cbc4
Nicolas Lambard <nicolasmclen2@gmail.com>
2024-01-25 16:18:04 +0100

    Ajout d'un tableau dans team_liste

--- app/Views/team/team_liste.php
+++ app/Views/team/team_liste.php
@@ -3,8 +3,37 @@
 <head>
 <title>Liste</title>
 </head>
+<style>
+table, 
+th, 
+td {
+    border-collapse: collapse;
+    border:1px solid black;
+	background-color:#ffc0cb;
+    
+}
+th {
+    background-color:#87CEEB;
+}
+        </style>
+
+
 <body>
 <p> Liste </p>
 <a href="/team/" >Retourner à team</a>
+
+<?php
+echo '  <table>
+<tr>
+<th> id_team </th> 
+<th> name </th>
+<th> comment </th>
+<th> date_create </th>
+<th> date_update </th>
+</tr>'; 
+
+
+
+?>
 </body>
 </html>
