4afd5fb15b1a953fe08f43176351d3426086c17b
NolanKef <nolan.keffala@gmail.com>
2024-01-25 13:57:44 +0100

    #Envoi

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+use App\Controllers\Right;
+
+$routes->get('right', [Right::class, 'index']);
+$routes->get('(:segment)', [Right::class, 'view']);
+
---
+++ app/Controllers/Right.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+class Right extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home')
+    {
+        if (! is_file(APPPATH . 'Views/right/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('right/' . $page)
+            . view('templates/footer');
+    }
+}
