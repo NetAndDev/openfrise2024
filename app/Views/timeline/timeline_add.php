11cefa82d4b334424b91e830ad156ca6218551e7
rymamr <amiarryma@gmail.com>
2024-01-25 15:05:53 +0100

    Installation projet

--- app/Views/timeline/timeline_add.php
+++ app/Views/timeline/timeline_add.php
@@ -11,6 +11,16 @@
 
         <a href="/timeline/"> Menu </a>
 
+      <form action="timeline_menu.php" method="post">
+
+      <label for="Timeline"> Ajouter une frise </label>
+      <select name="Timeline" id="Timeline">
+      <option value="">~ Timeline ~</option>
+    
+      </select><br><br>
+
+      <input type="submit" value="Ajouter">
+
 
 
         
