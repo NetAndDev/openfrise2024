583dad08d652d35a28ffb65e7939cd7031990ba0
clement musset <clement.musset79@gmail.com>
2024-01-25 16:53:40 +0100

    Intallation Projet

--- app/Views/hyphen/hyphen_add.php
+++ app/Views/hyphen/hyphen_add.php
@@ -5,7 +5,25 @@
 </head>
 <body>
 
-<p>add</p>
+<?php
+
+    $db = \Config\Database::connect();
+    $query   = $db->query('SELECT * FROM BDD_HYPHENS');
+    $results = $query->getResult();
+
+            echo 'Listes :' . '<br>';
+
+            echo '<form action="hyphen_liste.php" method="post">';
+            echo '<label for="liste-select"></label>';
+            echo '<input name="liste" id="liste">';
+
+            while ($row = $statement->fetch()) {
+            
+            }
+            echo '<input type="submit" value="Envoyer"> <br>';
+            echo '</form>';
+            
+?>
 
 </body>
 </html>
