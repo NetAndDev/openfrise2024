a80f48ebacf765260e8f16516bb0dd7c81069663
Remi Champion <championr@saintjo.org>
2024-01-25 11:10:11 +0100

    creation des pages view

---
+++ app/Controllers/Licence.php
@@ -0,0 +1,26 @@
+
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Exceptions\PageNotFoundException; // Add this line
+
+class Pages extends BaseController
+{
+    // ...
+
+    public function view($page = 'home')
+    {
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('licence/addlicence', $data)
+            . view('pages/' . $page)
+            . view('licence/listelicence')
+            . view('licence/menulicence');
+    }
+}
