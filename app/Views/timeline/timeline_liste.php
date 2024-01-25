6186a2839d7ec665f22f0fbc6bb9ade0edaee58e
rymamr <amiarryma@gmail.com>
2024-01-25 14:42:23 +0100

    Installation projet

--- app/Views/timeline/timeline_liste.php
+++ app/Views/timeline/timeline_liste.php
@@ -9,6 +9,25 @@
     <header>
         <h1>"Liste"</h1>
 
+<?php
+        echo '  <table>
+
+    <tr>
+    <th> id_timeline </th>
+    <th> label </th>
+    <th> sublabel </th>
+    <th> date_begin </th>
+    <th> date_end </th>
+    <th> is_public </th>
+    <th> comment </th>
+    <th> date_create </th>
+    <th> date_update </th>
+    <th> code_licence </th>
+    </tr>'; 
+
+    
+
+?>
 
 
 
