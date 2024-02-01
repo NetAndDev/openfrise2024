87cf0a99e72a4abe551b3e4a22b78001c92122d3
NolanKef <nolan.keffala@gmail.com>
2024-02-01 11:16:25 +0100

    #

--- app/Views/right/right_list.php
+++ app/Views/right/right_list.php
@@ -4,11 +4,27 @@
     <title>Liste</title>
 </head>
 
+<style>
+    table, tr, th {
+
+        border: 1px solid black;
+    }
+
+    th {
+
+        padding: 5px;
+    }
+</style>
+
 <body>
     <h1>Liste</h1>
+    <table>
     <tr>
-        <th>Utilisateur</th>
+        <th>ID</th>
+        <th>Nom</th>
+        <th>Prénom</th>
         <th>Rôle</th>
     </tr>
+    </table>
 </body>
 </html>
