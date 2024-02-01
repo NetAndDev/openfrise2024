306695048025b8aa53304e6f3965f4e09bcbb5fe
NolanKef <nolan.keffala@gmail.com>
2024-02-01 10:47:05 +0100

    #

--- app/Controllers/Right.php
+++ app/Controllers/Right.php
@@ -4,6 +4,11 @@ namespace App\Controllers;
 
 class Right extends BaseController
 {
+    public function index(): string
+    {
+        return view('right/right_index');
+    }
+
     public function add()
     {
         return view('right/right_add');
--- app/Views/right/right_list.php
+++ app/Views/right/right_list.php
@@ -2,8 +2,13 @@
 <html>
 <head>
     <title>Liste</title>
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
     <h1>Liste</h1>
+    <tr>
+        <th>Utilisateur</th>
+        <th>Rôle</th>
+    </tr>
 </body>
 </html>
