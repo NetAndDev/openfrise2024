abf3dbb9fe724c05ac9021fee67549693bcc340e
Remi Champion <championr@saintjo.org>
2024-02-01 16:50:34 +0100

    modif license

--- app/Controllers/License.php
+++ app/Controllers/License.php
@@ -1,55 +1,5 @@
 <?php
-/*
-namespace App\Controllers;
 
-use CodeIgniter\Exceptions\PageNotFoundException; // Add this line
-
-class License extends BaseController
-{
-    // ...
-
-   /*
-    public function menu()
-    {
-    return view('license/license_menu');
-    }
-    
-    public function add()
-    {
-    return view('license/[license_add');
-    }
-    public function list()
-    {
-    return view('license/license_list');
-    }
-*/
-/* public function find($id_user = false)
-{$license = license(licensemodel::class);
-$data['license'] = model->getuser($id_license);
-$page = "detail";
-
-
-if(! is_file(APPPATH . 'Views/license/' . $page . '.php')); {
-    // Whoops, we don't have a page for that!
-    throw new PageNotFoundException($page);
-}
-
-$data['title'] = ucfirst($page);
-$data['numero']= 123;
-
-
-return view('license/license' . $page,$data);
-}
-public function display($page = 'menu')
-    {
-        if (! is_file(APPPATH . 'Views/media/media_' . $page . '.php')) {
-            // Whoops, we don't have a page for that!
-            throw new PageNotFoundException($page);
-        }
-    {
-    return view('license/license_menu');
-    }
-} */
 
 namespace App\Controllers;
 
@@ -77,9 +27,8 @@ class License extends BaseController
     
         public function find($id_ser = false){
             $model = model(LicenseModel::class);
-            $data['license'] = $model->getLicense($id_user);
+            $data['license'] = $model->getLicense($id_license);
     
             return view('license/license_'.$page, $data);
         }
-    }        return view('license/license_'.$page, $data);
-    
+    }    
