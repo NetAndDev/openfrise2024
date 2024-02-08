d7488adf84f117d9d56d51349ff185d062154535
Axel Diril <dirila@saintjo.org~>
2024-02-08 13:21:05 +0100

    Affichage du tableau

--- app/Views/media/media_list.php
+++ app/Views/media/media_list.php
@@ -7,19 +7,24 @@
     <h1>Liste des médias</h1>
     <table>
         <tr>
-            <td>id_media</td>
-            <td>label</td>
-            <td>sublabel</td>
-            <td>legend</td>
-            <td>source</td>
-            <td>filename</td>
+            <th>Titre</th>
+            <th>Sous-titre</th>
+            <th>Légende</th>
+            <th>Source</th>
+            <th>Nom de fichier</th>
         </tr>
-    </table>
     <?php
         if (!empty($media) && is_array($media)){
+        
             foreach($media as $item){
                 if(is_array($item)){
-                    echo esc($item['label']);
+                    echo '<tr>';
+                    echo '<td>'.esc($item['label']).'</td>';
+                    echo '<td>'.esc($item['sublabel']).'</td>';
+                    echo '<td>'.esc($item['legend']).'</td>';
+                    echo '<td>'.esc($item['source']).'</td>';
+                    echo '<td>'.esc($item['filename']).'</td>';
+                    echo '</tr>';
                 }
             }
         }
@@ -27,4 +32,5 @@
             echo 'Aucun Média trouvé.';
         }
     ?>
+    </table>
 </body>
