c1c426148ede67211b40e9a2f97d0ce6e08c88dc
mathiasCarlier <mathiasc1811@gmail.com>
2024-03-14 10:16:31 +0100

    zd

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>CodeIgniter Tutorial</title>
-    <link href="<?=base_url()?>styles/defautl/style.css" rel="stylesheet" />
+    <link href="<?=base_url();?>styles/defautl/style.css" rel="stylesheet" <?=uniqid()?>/>
 </head>
 <body>
     <h1><?= esc($title) ?></h1>
