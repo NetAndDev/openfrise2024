77f969029c8fddd2cf8eaa5a482a25a785e054d7
mathiasCarlier <mathiasc1811@gmail.com>
2024-03-14 09:11:15 +0100

    zd

---
+++ app/Views/level/level_detail.php
@@ -0,0 +1,20 @@
+<?php
+
+        if (!empty($detail) && is_array($detail)){
+            echo '<h1>Détail de '.$detail['label'].'</h1>';
+
+            if(!empty($detail['comment'])){
+                echo '<p>Commentaire : '.$detail['comment'].'</p>';
+            }
+            else{
+                    echo '<p>Commentaire : Aucun commentaire</p>';
+            }
+
+            echo '<p>Date de création : '.$detail['date_create'].'</p>';
+
+            echo '<p>Dernière modification : '.$detail['date_update'].'</p>';
+        }
+        else{
+            echo "Cet enregistrement n'existe pas dans la table Medias.";
+        }
+    ?>
--- app/Views/level/level_menu.php
+++ app/Views/level/level_menu.php
@@ -1,4 +1,5 @@
     <a href="/add" >Formulaire pour les grades </a><br>
     <a href="/list" >Liste des rôles </a>
+    <a href="/detail" >Détail </a>
 
     
