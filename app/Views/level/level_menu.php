555f00c1f78109908d82ecfe988414e962446659
mathiasCarlier <mathiasc1811@gmail.com>
2024-03-14 09:26:21 +0100

    detail

--- app/Views/level/level_menu.php
+++ app/Views/level/level_menu.php
@@ -1,3 +1,3 @@
     <a href="/add" >Formulaire pour les grades </a><br>
-    <a href="/list" >Liste des rôles </a>
+    <a href="/list" >Liste des rôles </a><br>
     <a href="/detail" >Détail </a> 
