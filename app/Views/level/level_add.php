bcccb7103c23e19a22de2503955d45789b2b8083
mathiasCarlier <mathiasc1811@gmail.com>
2024-03-14 09:05:14 +0100

    qd

--- app/Views/level/level_add.php
+++ app/Views/level/level_add.php
@@ -1,9 +1,9 @@
 <p><a href="<?= env('app.baseURL') ?>menu/">Menu</a></p>
     
     <form action="<?= env('app.baseURL') ?>level/insert" method="POST">
-        <label for="label">Label :</label>
+        <label for="label">Nom du nouveau poste :</label>
         <input type="text" name='label' required><br>
-        <label for="comment">comment :</label>
+        <label for="comment">Commentaire :</label>
         <input type="text" name='comment' ><br><br>
 
         <input type="submit" value="Valider">
